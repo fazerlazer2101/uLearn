41b77b02b12e187b7b4ec2bc881eec8bac2d24f3
xammyboii <xavieryabados.09@gmail.com>
2023-12-02 22:54:10 -0600

    generated categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Routes for Course (Home)
