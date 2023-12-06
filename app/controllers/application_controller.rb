fa6ab184e5c6142ce5c62e12b341a8c945bbf6af
fazerlazer2101 <samferrer145@gmail.com>
2023-12-06 14:05:37 -0600

    Removed rack cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,2 @@
 class ApplicationController < ActionController::Base
-  after_action :allow_all_origins
-
-  def allow_all_origins
-    response.headers['Access-Control-Allow-Origin'] = 'https://checkout.stripe.com', 'http://localhost:3000/'
-  end
 end
