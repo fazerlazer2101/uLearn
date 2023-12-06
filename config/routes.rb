6b9c7766557172d2b76cd7d3deb8b34d153c3c29
xammyboii <xavieryabados.09@gmail.com>
2023-12-06 17:51:46 -0600

    updated routes for courses as main home page

--- config/routes.rb
+++ config/routes.rb
@@ -6,29 +6,29 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  # Routes for Course (Home)
-  root to: "home#index"
-  get "home/about"
-  resources :courses, only: %i[show]
+  # Routes for Course (main content)
+  root to: "courses#index"
+  get "courses/about"
+  resources :courses, only: %i[index show]
 
   # Routes for User
   get "user/show"
   devise_for :users
   post "/user/:id/update", to: "user#update"
+  get "/user/:id", to: "user#show"
+  resources :customer_infos, only: %i[show]
 
-  #Routes for cart
+  # Routes for Cart management
   post "/cart/:id/update", to: "cart#create"
   post "/cart/:id/delete", to: "cart#delete"
   post "/cart/:id/addRegion", to: "cart#addRegion"
   get "/cart", to: "cart#show"
 
-  #Stripe stuff
+  # Routes for Stripe
   scope "cart/checkout" do
     post "create", to: "checkout#create", as: "checkout_create"
     get "success", to: "checkout#success", as: "checkout_success"
     get "cancel", to: "checkout#cancel", as: "checkout_cancel"
   end
 
-  get "/user/:id", to: "user#show"
-  resources :customer_infos, only: %i[show]
 end
